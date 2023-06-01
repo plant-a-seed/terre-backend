a1aa2a062bf55646d72ec2adfaab931fb5163b62
Eugenio Berretta <euberdeveloper@gmail.com>
2023-06-02 00:09:32 +0200

    style(bot): make options lowercase
    Signed-off-by: Eugenio Berretta <euberdeveloper@gmail.com>

--- bot/index.ts
+++ bot/index.ts
@@ -3,23 +3,23 @@ import { Logger } from 'euberlog';
 import { Database } from '@/database.js';
 import { Bot } from '@/bot.js';
 
-import OPTIONS from '@/options.js';
+import options from '@/options.js';
 
 const logger = new Logger({
     scope: 'main',
-    debug: OPTIONS.debugLog
+    debug: options.debugLog
 });
 
 async function createBot() {
     logger.info('Creating bot...');
 
     const database = new Database({
-        url: OPTIONS.redis.url
+        url: options.redis.url
     });
     await database.open();
     logger.debug('Database instance created');
 
-    const bot = new Bot(OPTIONS.telegram.botToken, database);
+    const bot = new Bot(options.telegram.botToken, database);
     logger.success('Bot instance created!');
 
     return bot;
