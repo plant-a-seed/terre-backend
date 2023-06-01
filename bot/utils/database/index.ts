d43e06dbf3cc3472fe1004e2285170b6fdb5f8f7
Eugenio Berretta <euberdeveloper@gmail.com>
2023-06-01 23:51:27 +0200

    feat(bot): add database util
    Signed-off-by: Eugenio Berretta <euberdeveloper@gmail.com>

---
+++ bot/utils/database/index.ts
@@ -0,0 +1,47 @@
+import redis, { RedisClientType, RedisClientOptions } from 'redis';
+
+export class Database {
+    private static readonly CHATS_KEY = 'terreChatsKey';
+
+    private readonly client: RedisClientType;
+
+    constructor(options: RedisClientOptions) {
+        this.client = redis.createClient(options) as RedisClientType;
+    }
+
+    public async open(): Promise<void> {
+        await this.client.connect();
+    }
+
+    public async getChats(): Promise<number[]> {
+        const chatIds = await this.client.sMembers(Database.CHATS_KEY);
+        return chatIds.map(id => +id);
+    }
+
+    public async pushChat(chatId: number): Promise<void> {
+        await this.client.sAdd(Database.CHATS_KEY, String(chatId));
+    }
+
+    public async removeChat(chatId: number): Promise<void> {
+        await this.client.sRem(Database.CHATS_KEY, String(chatId));
+    }
+
+    public async resetChats(): Promise<void> {
+        await this.client.del(Database.CHATS_KEY);
+    }
+
+    public async importChats(chats: number[], overwrite = false): Promise<void> {
+        if (overwrite) {
+            await this.resetChats();
+        }
+        else {
+            for (const chatId of chats) {
+                await this.pushChat(chatId);
+            }
+        }
+    }
+
+    public async close(): Promise<void> {
+        await this.client.quit();
+    }
+}
