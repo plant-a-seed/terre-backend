4f3f809f8e90fb0481c92acca0414cf09b77b7a9
Eugenio Berretta <euberdeveloper@gmail.com>
2023-06-01 23:44:43 +0200

    feat(config): add env + config ts
    Signed-off-by: Eugenio Berretta <euberdeveloper@gmail.com>

---
+++ bot/options/index.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+import fs from 'node:fs';
+import path from 'node:path';
+
+const packageJson = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'package.json'), 'utf-8'));
+
+dotenv.config({
+    path: path.join(process.cwd(), '.env')
+});
+
+const redisHost = process.env.REDIS_HOST ?? 'localhost';
+const redisPort = process.env.REDIS_PORT ?? 6379;
+
+export default {
+    redis: {
+        host: redisHost,
+        port: redisPort,
+        url: `redis://${redisHost}:${redisPort}`
+    },
+    telegram: {
+        botToken: process.env.TELEGRAM_BOT_TOKEN as string
+    },
+    version: packageJson.version as string
+};
